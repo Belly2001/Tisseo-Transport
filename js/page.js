0c02718559c50739079b7e5f493354bc554bcaca
Belly2001 <ndangadonbellystar@gmail.com>
2026-01-23 11:52:45 +0100

    quelques modifications

---
+++ js/page.js
@@ -0,0 +1,18 @@
+const bouton = document.querySelector('button');
+const liste = document.querySelector('#liste-lignes');
+
+bouton.addEventListener('click', function() {
+    
+    fetch('https://data.toulouse-metropole.fr/api/explore/v2.1/catalog/datasets/ligne/records?limit=20')
+        .then(function(reponse) {
+            return reponse.json();
+        })
+        .then(function(donnees) {
+            // Pour chaque ligne, on crée un élément <li>
+            donnees.results.forEach(function(ligne) {
+                const li = document.createElement('li');
+                li.textContent = ligne.id_ligne;
+                liste.appendChild(li);
+            });
+        });
+});
